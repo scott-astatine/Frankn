8e8e1d9ce4afd8ed5522bfffa1bb96fcca920f3d
scott-astatine <scottastatine@gmail.com>
2026-05-17 08:48:56 +0530

    chore: silence debug log in media message handler

--- frankn/lib/services/client_rtc/rtc_message_handler.dart
+++ frankn/lib/services/client_rtc/rtc_message_handler.dart
@@ -338,7 +338,7 @@ mixin RtcMessageHandler on RtcClientBase {
 
   void _handleMediaUpdate(Map<String, dynamic> data) async {
     MediaUpdate media = MediaUpdate.fromJson(data);
-    log(data.toString());
+    // log(data.toString());
 
     final artStr = media.artData;
     if (artStr != null && artStr.startsWith('frankn-fs://')) {
